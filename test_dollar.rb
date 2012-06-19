5b745623ca84bb93a380a8937e612eb95bf80d82
sue445 <sue445@sue445.net>
2012-06-19 22:38:01 +0900

    add red test

--- test_dollar.rb
+++ test_dollar.rb
@@ -1,2 +1,11 @@
 require 'test/unit' 
 
+class DollarTest < Test::Unit::TestCase
+
+	def test_mulliplication
+		five = Dollar.new(5)
+	       	file.times(2)
+		assert_equal(10, five.amount)
+	end
+
+end
