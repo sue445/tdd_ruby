521584630d1fca5dae468ff5a51284fd4d5455f5
sue445 <sue445@sue445.net>
2012-06-23 20:07:16 +0900

    refactor test

--- test_money.rb
+++ test_money.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require_relative 'dollar'
 require_relative 'franc'
 
-class DollarTest < Test::Unit::TestCase
+class MoneyTest < Test::Unit::TestCase
 
 	def test_mulliplication
 		five = Money.dollar(5) 
@@ -16,5 +16,11 @@ class DollarTest < Test::Unit::TestCase
 		assert Money.franc(5) == Money.franc(5)
 		assert Money.franc(5) != Money.franc(6)
 		assert Money.franc(5) != Money.dollar(5)
+	end
+
+	def test_franc_mulliplication
+		five = Money.franc(5)
+		assert_equal(Money.franc(10), five.times(2))
+		assert_equal(Money.franc(15), five.times(3))
 	end
 end
