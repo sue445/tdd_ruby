b7044d2dbd5031021ed074e16d558eb69674cbfb
sue445 <sue445@sue445.net>
2012-06-23 20:01:03 +0900

    P37 done

--- test_dollar.rb
+++ test_dollar.rb
@@ -6,15 +6,15 @@ class DollarTest < Test::Unit::TestCase
 
 	def test_mulliplication
 		five = Money.dollar(5) 
-		assert_equal(Dollar.new(10), five.times(2))
-		assert_equal(Dollar.new(15), five.times(3))
+		assert_equal(Money.dollar(10), five.times(2))
+		assert_equal(Money.dollar(15), five.times(3))
 	end
 
 	def test_equality
-		assert Dollar.new(5) == Dollar.new(5)
-		assert Dollar.new(5) != Dollar.new(6)
-		assert Franc.new(5) == Franc.new(5)
-		assert Franc.new(5) != Franc.new(6)
-		assert Franc.new(5) != Dollar.new(5)
+		assert Money.dollar(5) == Money.dollar(5)
+		assert Money.dollar(5) != Monery.dollar(6)
+		assert Money.franc(5) == Money.franc(5)
+		assert Money.franc(5) != Money.franc(6)
+		assert Money.franc(5) != Money.dollar(5)
 	end
 end
--- test_franc.rb
+++ test_franc.rb
@@ -3,9 +3,9 @@ require_relative 'franc'
 
 class FrancTest < Test::Unit::TestCase
 
-	def test_mulliplication
-		five = Franc.new(5)
-		assert_equal(Franc.new(10), five.times(2))
-		assert_equal(Franc.new(15), five.times(3))
+	def test_franc_mulliplication
+		five = Money.franc(5)
+		assert_equal(Money.franc(10), five.times(2))
+		assert_equal(Money.franc(15), five.times(3))
 	end
 end
